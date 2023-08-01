9abdc7a122a10799199a89886f7273f5de62817f
Mikey <michalgordon92@gmail.com>
2023-07-31 18:13:20 -0600

    create constants folder and move ticketTypes to it

---
+++ src/components/constants/index.js
@@ -0,0 +1,32 @@
+
+export const ticketTypes = [
+    {
+      type: "bud",
+      perks: ["8 Entrances for live or online lectures", "1 workshop"],
+      price: 144,
+      signUpNeeded: true,
+      color: "green",
+    },
+    {
+      type: "bloom",
+      perks: [
+        "10 live/online lectures",
+        "5 workshops",
+        "Mingling zone entrance",
+      ],
+      price: 256,
+      signUpNeeded: false,
+      color: "yellow",
+    },
+    {
+      type: "bouquet",
+      perks: [
+        "Unlimited live/online lectures",
+        "Unlimited workshops",
+        "Mingling zone entrance",
+      ],
+      price: 356,
+      signUpNeeded: false,
+      color: "pink",
+    },
+  ];
