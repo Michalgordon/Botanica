716a42e3bf56d0f94a959e4714dfc0f92471dd0e
Mikey <michalgordon92@gmail.com>
2023-10-04 10:14:01 -0700

    add navbar data to constants and add handle click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import { Globe } from "../assets";
+import { navbarItems } from "../constants";
 
 const Navbar = () => {
+  function handleClick(navTo) {
+    const element = document?.getElementById(navTo);
+    element?.scrollIntoView({
+      block: "start",
+      behavior: "smooth",
+    });
+  }
+
   return (
     <nav className=" invisible relative z-20 sm:visible w-full p-2">
       <ul className="w-full flex flex-start gap-4">
-        <li className="list-none text-3xl font-thin rounded-full bg-green text-black px-24">
+        <li className="list-none uppercase tracking-widest font-bold text-3xl rounded-full bg-green text-black px-24">
           Botanica
         </li>
-        <li className="list-none cursor-pointer text-3xl rounded-full px-4 hover:bg-white">
-          Speakers
-        </li>
-        <li className="list-none cursor-pointer text-3xl rounded-full px-4 hover:bg-white">
-          Agenda
-        </li>
-        <li className="list-none cursor-pointer text-3xl rounded-full px-4 hover:bg-white">
-          Q&A
-        </li>
-        <li className="list-none cursor-pointer text-3xl rounded-full px-4 hover:bg-white">
-          Tickets
-        </li>
+
+        {navbarItems.map((navbarItem) => (
+          <li
+            key={navbarItem.title}
+            onClick={() => handleClick(navbarItem.navTo)}
+            className="list-none cursor-pointer uppercase text-3xl rounded-full px-4 hover:bg-white"
+          >
+            {navbarItem.title}
+          </li>
+        ))}
         <ul className="list-none flex flex-start gap-4 ml-auto">
-          <li className="list-none cursor-pointer text-3xl rounded-full px-4 hover:bg-white">
+          <li className="list-none cursor-pointer uppercase text-3xl rounded-full px-4 hover:bg-white">
             Search
           </li>
-          <Globe fill="black" stroke="none" className="w-[42px] h-[42px]" />
+          <div className="w-[52px] hover:bg-white hover:rounded-full">
+            <Globe className="w-[42px] h-[42px] fill-black hover" />
+          </div>
         </ul>
       </ul>
     </nav>
