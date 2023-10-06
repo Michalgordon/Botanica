eb3395112fbcacfa52b7b8ad850e0285cf925a37
Mikey <michalgordon92@gmail.com>
2023-10-06 16:10:00 -0700

    mobile nav bar icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,5 +7,10 @@ import Sun from "./sun.svg?react"
 import Star from "./star.svg?react"
 import ArrowDown from "./arrow-down.svg?react"
 import HamburgerClosed from "./hamburger-closed.svg?react"
+import Agenda from "./agenda.svg?react"
+import MsgBubble from "./msg-bubble.svg?react"
+import QuestionMark from "./question-mark.svg?react"
+import Ticket from "./ticket.svg?react"
+import Search from "./search.svg?react"
 
-export {Globe, headshot, Flower, Tulip, HeroTitle, Sun, Star, ArrowDown,HamburgerClosed}
+export {Globe, headshot, Flower, Tulip, HeroTitle, Sun, Star, ArrowDown,HamburgerClosed, Agenda, MsgBubble, QuestionMark, Ticket, Search}
