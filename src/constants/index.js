347338240a2f938c91a6a6614bd7656df6f563ab
Mikey <michalgordon92@gmail.com>
2023-10-29 16:26:27 -0700

    constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -163,6 +163,8 @@ export const QA_QUESTIONS = [
     },
 
   ]
+
+
   export const FOOTER_ITEMS = [
 {title: 'terms & conditions'},
 {title: 'privacy'},
@@ -170,3 +172,144 @@ export const QA_QUESTIONS = [
 {title: 'about'},
 {title: 'contact'},
   ]
+
+  export const AGENDA_LOCATIONS = {
+    1: "terra hall",
+    2: "main hall",
+    3: "workshop"
+
+  }
+  
+  export const AGENDA_EVENTS = [
+  {
+    title: "Growth Journey",
+    description: "How can your kids get involved in the homestead chores.",
+    location: "1",
+    dateTime: "2023-03-01T09:30",
+    duration: 1.5,
+    available: 15,
+    booked: 15,
+    id: "1"
+  },
+  {
+    title: "Of Plants & Pollinators",
+    description: "How to make your garden into heaven for pollinator’s.",
+    location: "2",
+    dateTime: "2023-03-01T09:30",
+    duration: 1.5,
+    available: 15,
+    booked: 10,
+    id: "2"
+
+  },
+  {
+    title: "Farm Model Building Part 1",
+    description: "Taking the first steps in planing your homestead dream.",
+    location: "3",
+    dateTime: "2023-03-01T09:30",
+    duration: 2.5,
+    available: 15,
+    booked: 10,
+    id: "3"
+
+  },
+  {
+    title: "Climate Resistance",
+    description: "The story of a climate activists and the impact they make.",
+    location: "1",
+    dateTime: "2023-03-01T13:00",
+    duration: 1.5,
+    available: 15,
+    booked: 10,
+    id: "4"
+
+  },
+  {
+    title: "Hydro DIY",
+    description: "How to implement hydrophobic systems at (almost) any scale.",
+    location: "2",
+    dateTime: "2023-03-01T13:00",
+    duration: 1.5,
+    available: 15,
+    booked: 15,
+    id: "5"
+
+  },
+  {
+    title: "Farm Model Building Part 2",
+    description: "How can your kids get involved in the homestead chores.",
+    location: "3",
+    dateTime: "2023-03-01T13:00",
+    duration: 3,
+    available: 15,
+    booked: 10,
+    id: "6"
+  },
+  {
+    title: "Self-Sufficiency Part 1",
+    description: "Learn how to make your homestead independent.",
+    location: "1",
+    dateTime: "2023-03-02T09:30",
+    duration: 1.5,
+    available: 15,
+    booked: 15,
+    id: "7"
+  },
+  {
+    title: "Buying Land",
+    description: "How to find the right land for your homestead.",
+    location: "2",
+    dateTime: "2023-03-02T09:30",
+    duration: 1.5,
+    available: 15,
+    booked: 10,
+    id: "8"
+
+  },
+  {
+    title: "Farm Model Building Part 3",
+    description: "Taking the first steps in planing your homestead dream.",
+    location: "3",
+    dateTime: "2023-03-02T09:30",
+    duration: 2.5,
+    available: 15,
+    booked: 10,
+    id: "9"
+
+  },
+  {
+    title: "Self-Sufficiency Part 2",
+    description: "Learn how to make your homestead independent.",
+    location: "1",
+    dateTime: "2023-03-02T13:00",
+    duration: 1.5,
+    available: 15,
+    booked: 10,
+    id: "10"
+
+  },
+  {
+    title: "Hydro DIY",
+    description: "How to implement hydrophobic systems at (almost) any scale.",
+    location: "2",
+    dateTime: "2023-03-02T13:00",
+    duration: 1.5,
+    available: 15,
+    booked: 15,
+    id: "11"
+
+  },
+  {
+    title: "Farm Model Building Part 4",
+    description: "How can your kids get involved in the homestead chores.",
+    location: "3",
+    dateTime: "2023-03-02T13:00",
+    duration: 3,
+    available: 15,
+    booked: 10,
+    id: "12"
+  },
+];
+
+  export const AGENDA_DATES = [{key: 1, value: "2023-03-01T00:00"}, {key: 2, value:"2023-03-02T00:00"}, {key: 3, value:"2023-03-03T00:00"}]
+  
