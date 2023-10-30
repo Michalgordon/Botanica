fa7492829790b54353c6d6d9c6b70afa7dccfb33
Mikey <michalgordon92@gmail.com>
2023-10-29 17:23:49 -0700

    fix position of carousel effect

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -44,11 +44,11 @@ const Speakers = () => {
       id="speakers"
       className="relative section--x-scrollable section--col md:section z-10 bg-white"
     >
-      <svg className="svg">
+      {/* <svg className="svg">
         <clipPath id="my-clip-path" clipPathUnits="objectBoundingBox">
           <path d="M0.363,0.325 L0.556,0.009 L0.743,0.325 L0.744,0.327 L0.746,0.325 L0.999,0.011 C0.999,0.034,1,0.059,1,0.085 C1,0.246,1,0.453,0.971,0.63 C0.946,0.735,0.904,0.829,0.834,0.897 C0.765,0.965,0.667,1,0.532,1 C0.396,1,0.296,0.97,0.222,0.902 C0.148,0.835,0.101,0.741,0.07,0.636 C0.016,0.458,0.011,0.249,0.007,0.086 C0.007,0.059,0.006,0.034,0.005,0.01 L0.36,0.325 L0.362,0.327 L0.363,0.325"></path>
         </clipPath>
-      </svg>
+      </svg> */}
       <SectionHeader
         header="Speakers"
         subHeader="Get to know who’s coming"
@@ -58,7 +58,7 @@ const Speakers = () => {
           <SpeakerItem key={speaker.id} speaker={speaker}></SpeakerItem>
         ))}
       </ul>
-      <div className="sm:hidden ss:absolute ss:h-[26px] ss:w-full ss:rounded-[50%] ss:top-[484px] ss:left-0 ss:bg-white">
+      <div className="sm:hidden ss:absolute ss:h-[26px] ss:w-full ss:rounded-[50%] ss:top-[462px] ss:left-0 ss:bg-white">
         {/*  carousel effect for mobile */}
       </div>
       <div className="md:hidden absolute bg-gradient-to-r from-transparent to-gray right-0 top-0 w-12 h-full ">
