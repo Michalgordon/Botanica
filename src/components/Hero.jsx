54f75c3ec9402eff0f956ad8ffd01a61d65b814e
Mikey <michalgordon92@gmail.com>
2023-11-06 15:37:54 -0800

    added animation to scroll down arrow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ import { ArrowDown } from "../assets/icons";
 const Hero = () => {
   return (
     <section id="home" className="section relative h-200">
-      <Tulip className="absolute top-[-33px] left-[-40px] lg:left-[50%] lg:top-[-82px] rotate-45 fill-pink opacity-60 min-w-[255px] w-1/5"></Tulip>
-      <Flower className="absolute left-[-80px] bottom-[-120px] lg:top-[539px] lg:left-[633.95px] rotate-53 fill-green opacity-60 min-w-[261px] w-1/5"></Flower>
-      <Star className="absolute bottom-5 left-[200px] lg:bottom-[10%] lg:left-[1400px] fill-yellow opacity-60 min-w-[392px] w-[30%]"></Star>
+      <Tulip className="absolute top-[-33px] left-[-40px] md:left-[50%] md:top-[-82px] rotate-45 fill-pink opacity-60 min-w-[255px] w-1/5"></Tulip>
+      <Flower className="absolute left-[-80px] bottom-[-120px] md:top-[539px] md:left-[633.95px] rotate-53 fill-green opacity-60 min-w-[261px] w-1/5"></Flower>
+      <Star className="absolute bottom-5 left-[200px] md:bottom-[10%] md:left-[1400px] fill-yellow opacity-60 min-w-[392px] w-[30%]"></Star>
 
-      <div className="flex flex-col md:pt-14 gap-12 sm:gap-14 md:gap-16 opacity-95">
+      <div className="flex flex-col md:pl-36 md:pt-14 lg:pl-0 gap-12 sm:gap-14 md:gap-16 opacity-95">
         <h3 className="text-base sm:text-xl md:text-2xl">
           INTERNATIONAL BOTANY SOCIETY, PLANTING SEEDS FOR A BETTER FUTURE
         </h3>
@@ -18,11 +18,11 @@ const Hero = () => {
           Get Your Ticket Here!
         </button>
       </div>
-      <div className="absolute bottom-10 md:bottom-14 right-20 sm:right-36 md:right-80 ">
-        <p className="uppercase tracking-wide text-base sm:text-xl sm:tracking-wide sm:pb-3 md:tracking-widest md:text-2xl md:pb-5">
+      <div className="absolute bottom-10 md:bottom-28 right-20 md:right-36 lg:right-80 ">
+        <p className="uppercase tracking-wide text-base sm:text-xl sm:tracking-wide  md:tracking-widest md:text-2xl ">
           scroll down
         </p>
-        <ArrowDown className="fill-black w-30 sm:w-40 md:w-52"></ArrowDown>
+        <ArrowDown className="fill-black w-30 sm:w-40 md:w-52 mt-6 md:mt-10 animate-bounce"></ArrowDown>
       </div>
     </section>
   );
